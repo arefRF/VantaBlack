40710678c078e09e5ec4f3789fc7e5af32a033b2
Reza Mirzaee <rmirzaee95@gmail.com>
2016-12-15 12:44:25 +0330

    Lean and Walk INterface added

--- Assets/scripts/Interface.cs
+++ Assets/scripts/Interface.cs
@@ -8,139 +8,77 @@ public class Interface : MonoBehaviour {
     
     Database database;
     
-    private Direction lean;
+    private Direction lean_direction;
     private float camera_speed = 0.05f;
     private bool is_lean;
+    private bool ability_use;
 	// Use this for initialization
 	void Start () {
         database = Starter.GetDataBase();
         engine = Starter.GetEngine();        
         is_lean = false;
+        ability_use = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (database.state == State.Idle)
         {
-            if (!Input.GetKeyDown(KeyCode.Space))
-            {
-                if (Input.GetKeyDown(KeyCode.RightArrow))
-                {
-                    if (!is_lean)
-                    {
-                        if (engine.ArrowKeyPressed(Direction.Right))
-                        {
-                            database.state = State.Idle;
-                            return;
-                        }
-                        engine.Gengine._lean_right();
-                        is_lean = true;
-                        lean = Direction.Right;
-                    }
-
-                }
-                else if (Input.GetKeyDown(KeyCode.LeftArrow))
-                {
-
-                    if (!is_lean)
-                    {
-                        if (engine.ArrowKeyPressed(Direction.Left))
-                        {
-                            database.state = State.Idle;
-                            return;
-                        }
-                        engine.Gengine._lean_left();
-                        is_lean = true;
-                        lean = Direction.Left;
-                    }
-                }
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                {
-                    if(!is_lean)
-                    if (true)//(new Vector2(0, -1)))
-                        {
-                        engine.move(Direction.Down);
-                        engine.EndTurn();
-                    }
-                }
-                else if (Input.GetKeyDown(KeyCode.UpArrow))
-                {
-                    if (!is_lean)
-                    {
-                        engine.Gengine._lean_top();
-                        lean = Direction.Up;
-                        is_lean = true;
-                    }
-
-                }
-
-                else if (Input.GetKeyUp(KeyCode.UpArrow))
-                {
-                    if (is_lean && lean==Direction.Up)
-                    {
-                        engine.Gengine._lean_top_undo();
-                        is_lean = false;
-                    }
-                }
-                /// if released it should undo the lean
-                else if (Input.GetKeyUp(KeyCode.RightArrow))
-                {
-                    if (is_lean && lean == Direction.Right)
-                    {
-                        engine.Gengine._lean_right_undo();
-                        is_lean = false;
-                    }
-                }
-
-                else if (Input.GetKeyUp(KeyCode.LeftArrow))
-                    if (is_lean && lean==Direction.Left)
-                    {
-                        engine.Gengine._lean_left_undo();
-                        is_lean = false;
-                    }
-            }
-         
-            else if (Input.GetKeyDown(KeyCode.Space))
-            {
-                    engine.Act();
-                    engine.EndTurn();
-            }
-
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-               
-                
-            }
-            else if (Input.GetKeyUp(KeyCode.R))
-            {
-                if (engine.snapshotunits.Count != 0)
-                {
-                    Wall.print("some changes not saved to snapshot");
-                }
-                engine.Undo();
-            }
-            else if (Input.GetKeyDown(KeyCode.M))
-            {
-                GameObject.Find("Map").GetComponent<MapController>()._click();
-            }
-            else if (Input.GetKey(KeyCode.L))
-            {
-                Set_Camera(new Vector2(camera_speed, 0));
-            }
-            else if (Input.GetKey(KeyCode.J))
-            {
-                Set_Camera(new Vector2(-camera_speed, 0));
-            }
-            else if (Input.GetKey(KeyCode.I))
-            {
-                Set_Camera(new Vector2(0, camera_speed));
-            }
-            else if (Input.GetKey(KeyCode.K))
-            {
-                Set_Camera(new Vector2(0, -camera_speed));
-            }
+            if (!ability_use)
+                Get_Arrows();
+            else
+                Get_Ability();
         }
     }
+
+
+
+    private void Lean()
+    {
+        Debug.Log("Leaning");
+    }
+
+    private void Get_Ability()
+    {
+
+    }
+    private void Get_Arrows()
+    {
+
+        //Take Arrows
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            if (!engine.ArrowKeyPressed(Direction.Right))
+            {
+                is_lean = true;
+                lean_direction = Direction.Right;
+                Lean();
+            }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (!engine.ArrowKeyPressed(Direction.Left))
+            {
+                is_lean = true;
+                lean_direction = Direction.Left;
+                Lean();
+            }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            if (!engine.ArrowKeyPressed(Direction.Down))
+            {
+                is_lean = true;
+                lean_direction = Direction.Up;
+                Lean();
+            }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            if (!engine.ArrowKeyPressed(Direction.Up))
+            {
+                is_lean = true;
+                lean_direction = Direction.Up;
+                Lean();
+            }
+    }
+
+
 
 
     private bool _lean_action()
