aa140ef2b44a44856ab2148ba5c4a2fee0367c3d
Reza Mirzaee <rmirzaee95@gmail.com>
2016-12-22 16:55:04 +0330

    Rotation animation added

--- Assets/scripts/GraphicalEngine/PlayerGraphics.cs
+++ Assets/scripts/GraphicalEngine/PlayerGraphics.cs
@@ -3,6 +3,23 @@ using System.Collections;
 
 public class PlayerGraphics : MonoBehaviour {
     public float move_time = 1;
+    private Animator animation;
+
+    void Start()
+    {
+        animation = GetComponent<Animator>();
+    }
+
+    private void Rotate_Left_Finished()
+    {
+        animation.SetInteger("State", 0);
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+    }
+
+    private void Rotate_Right_Finished()
+    {
+        animation.SetInteger("State", 0);
+    }
     public void Player_Move(GameObject player,Direction dir)
     {
         Vector2 end = (Vector2)player.transform.position + (Vector2)Toolkit.DirectiontoVector(dir);
@@ -11,10 +28,11 @@ public class PlayerGraphics : MonoBehaviour {
 
     public void Player_Change_Direction(GameObject player,Direction dir)
     {
-        if(dir==Direction.Right)
-            player.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
-        else if(dir== Direction.Left)
-            player.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+
+        if (dir == Direction.Right)
+            animation.SetInteger("State", 1);
+        else if (dir == Direction.Left)
+            animation.SetInteger("State", -1);
     }
 
     public void Player_Roll(GameObject player,Direction dir,int number)
