c2a71030a3f5677529784e500dad4faadf044368
aref <aref>
2017-01-17 19:24:33 +0330

    api unit completed

--- Assets/scripts/LogicalEngine/APIUnit.cs
+++ Assets/scripts/LogicalEngine/APIUnit.cs
@@ -1,23 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class APIUnit {
     LogicalEngine engine;
 
-    public void MovePlayer(Player player, Direction dir)
+    public void Move(Unit unit, Direction dir)
     {
-
-    }
-	
-    public void MoveUnit(Unit unit, Direction d)
-    {
-
+        if(unit is Player)
+        {
+            engine.MovePlayer((Player)unit, Toolkit.VectorSum(unit.position, Toolkit.DirectiontoVector(dir)));
+        }
+        else
+        {
+            engine.MoveUnit(unit, Toolkit.VectorSum(unit.position, Toolkit.DirectiontoVector(dir)));
+        }
     }
 
     public void Lean(Player player, Direction dir)
     {
-
+        engine.Lean(player, dir);
     }
 
-
+    public List<Unit> GetUnits(Unit unit, Direction dir)
+    {
+        return engine.GetUnits(Toolkit.VectorSum(Toolkit.DirectiontoVector(dir), unit.position));
+    }
 }
--- Assets/scripts/LogicalEngine/LogicalEngine.cs
+++ Assets/scripts/LogicalEngine/LogicalEngine.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class LogicalEngine {
 
     APIGraphic apigraphic;
     APIInput apiinput;
     APIUnit apiunit;
+    Database database;
 
     public LogicalEngine()
     {
 
     }
-    
 
+    public void MoveUnit(Unit unit, Vector2 position)
+    {
+
+    }
+
+    public void MovePlayer(Player player, Vector2 position)
+    {
+
+    }
+
+    public void Lean(Player player, Direction direction)
+    {
+
+    }
+
+    public List<Unit> GetUnits(Vector2 position)
+    {
+        return database.units[(int)position.x, (int)position.y];
+    }
 }
