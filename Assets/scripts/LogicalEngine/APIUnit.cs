c144634252c47cf482fbd032cb36315db9bdb660
aref <aref>
2017-01-17 18:58:30 +0330

    logical engine and api created

---
+++ Assets/scripts/LogicalEngine/APIUnit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class APIUnit {
+    LogicalEngine engine;
+
+    public void MovePlayer(Player player, Direction dir)
+    {
+
+    }
+	
+    public void MoveUnit(Unit unit, Direction d)
+    {
+
+    }
+
+    public void Lean(Player player, Direction dir)
+    {
+
+    }
+
+
+}
---
+++ Assets/scripts/LogicalEngine/LogicalEngine.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicalEngine {
+
+    APIGraphic apigraphic;
+    APIInput apiinput;
+    APIUnit apiunit;
+
+    public LogicalEngine()
+    {
+
+    }
+    
+
+}
