5f0cc1382f22d584dbc8cea6745cc16cb8753a43
aref <aref>
2017-02-16 14:56:40 +0330

    fuel bug fix

--- Assets/scripts/ParentScript.cs
+++ Assets/scripts/ParentScript.cs
@@ -4,10 +4,8 @@ using System.Collections;
 public class ParentScript : MonoBehaviour {
 
     public bool movelock { get; set; }
-    private bool temp;
 	// Use this for initialization
 	void Start () {
         movelock = false;
-        temp = movelock;
 	}
 }
