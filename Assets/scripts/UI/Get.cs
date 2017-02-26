536d50bfffc6dece95e451e0110caa482d0bc709
Noise <rmirzaee95@gmail.com>
2017-02-26 18:59:25 +0330

    UI get added

---
+++ Assets/scripts/UI/Get.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Get : MonoBehaviour {
+
+    public GameObject hud;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
