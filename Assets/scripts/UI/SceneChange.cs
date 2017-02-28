d0ac72e6fdfbfa869468b62524e2585d4d1b898b
Noise <rmirzaee95@gmail.com>
2017-02-28 17:42:43 +0330

    Scene Change script added

---
+++ Assets/scripts/UI/SceneChange.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+    public string scene_name;
+    void OnTriggerEnter2D()
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
