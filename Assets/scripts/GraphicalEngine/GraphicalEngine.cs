745b15614b88776566b64707a13645e4fa0b4323
aref <aref>
2017-03-05 19:04:29 +0330

    gate buf gix

--- Assets/scripts/GraphicalEngine/GraphicalEngine.cs
+++ Assets/scripts/GraphicalEngine/GraphicalEngine.cs
@@ -157,7 +157,7 @@ public class GraphicalEngine : MonoBehaviour {
     {
         if(gate.abilities.Count != 0 && gate.abilities[0].abilitytype == AbilityType.Key)
         {
-            gate.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load("Doors/door part 6",typeof(Sprite));
+            gate.transform.GetChild(6).GetComponent<SpriteRenderer>().sprite = (Sprite)Resources.Load("Doors/Door 2 ON",typeof(Sprite));
         }
     }
     
