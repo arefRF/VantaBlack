8337ffc3f009552313351c17c631efb0347bc5c0
aref <aref>
2017-04-20 15:08:37 +0430

    nth

--- Assets/scripts/Ability/Jump.cs
+++ Assets/scripts/Ability/Jump.cs
@@ -20,6 +20,7 @@ public class Jump : Ability {
 
     public void Action(Player player, Direction direction)
     {
+        Debug.Log("here");
         Starter.GetDataBase().StopTimer();
         player.state = PlayerState.Busy;
         player.currentAbility = this;
