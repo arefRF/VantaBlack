b53fb349dd5277d43313e65b4c6573a47ed78b8f
aref <aref>
2017-04-23 17:52:12 +0430

    nth

--- Assets/scripts/LogicalEngine/LogicalEngine.cs
+++ Assets/scripts/LogicalEngine/LogicalEngine.cs
@@ -53,9 +53,12 @@ public class LogicalEngine {
         //Applygravity();
     }
 
-    public void MoveDone(Player player, Direction direction)
+    public void MoveDone(Player player)
     {
-
+        /*switch (direction)
+        {
+            case Direction.Right: 
+        }*/
     }
 
     public bool MoveUnit(Unit unit, Direction dir)
