411434cc47bccec80da699c3dd371ec5c92bac62
Reza Mirzaee <rmirzaee95@gmail.com>
2017-05-11 08:29:35 +0430

    Eye Moves

--- Assets/scripts/Extra/ActionUponCollide/OpenEyesUponCollide.cs
+++ Assets/scripts/Extra/ActionUponCollide/OpenEyesUponCollide.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System;
 
 public class OpenEyesUponCollide : MonoBehaviour{
-    public List<GameObject> eyepart;
+    public List<GameObject> parentEye;
     bool opened;
 	// Use this for initialization
 	void Start () {
@@ -20,8 +20,20 @@ public class OpenEyesUponCollide : MonoBehaviour{
         if (!opened && col.tag == "Player")
         {
             opened = true;
+            for(int i = 0; i < parentEye.Count; i++)
+            {
+                StartCoroutine(parentEye[i].transform.GetChild(0).GetComponent<EyeMove>().EyeSmoothMove());
+            }
+            /*
+
+
             for (int i = 0; i < eyepart.Count; i++)
-                eyepart[i].GetComponent<SpriteRenderer>().enabled = false;
+                eyepart[i].GetComponent<SpriteRenderer>().enabled = false; */
         }
+    }
+
+    public void UnlockPArent()
+    {
+       GetComponent<ParentScript>().movelock = true;
     }
 }
