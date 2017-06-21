2dd646f3fdcac2ab834468dfac26b1f5b733b863
aref <aref>
2017-06-21 17:17:02 +0430

    lean bug fix && jump 3 and 4 accelerate bug fix && playerchange direction added

---
+++ Assets/scripts/Colliders/Col_PlayerAdder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Col_PlayerAdder : MonoBehaviour {
+
+    public Player player;
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            if ((Vector2)col.transform.position == col.gameObject.GetComponent<Unit>().position)
+            {
+                Starter.GetDataBase().player.Add(player);
+                player.ApplyGravity();
+                GetComponent<BoxCollider2D>().isTrigger = false;
+            }
+        }
+    }
+}
