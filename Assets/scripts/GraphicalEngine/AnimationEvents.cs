785ae859cab199c964b66d893ac92d2ba12e7ca7
Reza Mirzaee <rmirzaee95@gmail.com>
2017-07-11 11:15:16 +0430

    Animation Invis fix

--- Assets/scripts/GraphicalEngine/AnimationEvents.cs
+++ Assets/scripts/GraphicalEngine/AnimationEvents.cs
@@ -16,6 +16,7 @@ public class AnimationEvents : MonoBehaviour {
     private void InTheBranch()
     {
         transform.localScale = new Vector2(1, 1);
+        transform.parent.GetComponent<Player>().MoveToBranchFinished();
     }
 
     private void LeanUndoFinished()
@@ -34,6 +35,6 @@ public class AnimationEvents : MonoBehaviour {
         transform.localScale = new Vector2(0, 0);
         transform.parent.transform.position = transform.parent.GetComponent<Player>().position;
         Starter.GetEngine().apigraphic.MovePlayerFinished(transform.parent.gameObject);
-        transform.parent.GetComponent<Player>().MoveToBranchFinished();
+        
     }
 }
