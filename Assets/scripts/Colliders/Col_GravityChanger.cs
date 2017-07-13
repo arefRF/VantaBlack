eaf713c22760bbeb6ec6ede501f05d372b7374f1
aref <aref>
2017-07-13 11:04:08 +0430

    sadv

--- Assets/scripts/Colliders/Col_GravityChanger.cs
+++ Assets/scripts/Colliders/Col_GravityChanger.cs
@@ -12,11 +12,11 @@ public class Col_GravityChanger : MonoBehaviour {
             if ((Vector2)col.transform.position == col.gameObject.GetComponent<Unit>().position)
             {
                 Player player = col.gameObject.GetComponent<Player>();
+                Starter.GetDataBase().SetGravity(GravityDirection);
                 int zrot = 0;
                 if (player.GetGravity() == Direction.Up)
                     zrot = 180;
                 player.transform.rotation = Quaternion.Euler(player.transform.rotation.x, player.transform.rotation.y, zrot);
-                Starter.GetDataBase().SetGravity(GravityDirection);
                 Starter.GetEngine().Applygravity();
             }
         }
