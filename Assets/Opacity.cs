ebf747ad423cc73d622d00c3a6114c4f381ba8ee
Reza Mirzaee <rmirzaee95@gmail.com>
2017-08-09 15:54:24 +0430

    LaserHit and UnHit api added

--- Assets/Opacity.cs
+++ Assets/Opacity.cs
@@ -2,12 +2,22 @@
 using System.Collections;
 
 public class Opacity : MonoBehaviour {
-
+    float opa = 1;
 	// Use this for initialization
 	void Start () {
-        GetComponent<Renderer>().material.SetFloat("_Cutoff", 0.5f);
+        GetComponent<Renderer>().material.SetFloat("_Cutoff",1f);
+        StartCoroutine(fill());
 	}
 	
+    IEnumerator fill()
+    {
+        while (opa > 0)
+        {
+            GetComponent<Renderer>().material.SetFloat("_Cutoff", opa);
+            opa = opa - 0.001f;
+            yield return null;
+        }
+    }
 	// Update is called once per frame
 
 }
