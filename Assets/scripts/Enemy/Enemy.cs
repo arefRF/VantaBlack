4490b4b35adf7f838da4f713d445e457760c210e
aref <valipour.aref@gmail.com>
2017-08-24 11:38:41 +0430

    enemy begin

--- Assets/scripts/Enemy/Enemy.cs
+++ Assets/scripts/Enemy/Enemy.cs
@@ -2,26 +2,31 @@
 using System.Collections.Generic;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
     public List<Direction> MoveDirections;
     private Vector2 MoveToPosition;
     private Coroutine coroutine;
-	void Start () {
-	
-	}
-	
-	
-	void Update () {
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
         for (int i = 0; i < MoveDirections.Count; i++)
             CheckPlayer(MoveDirections[i]);
-	}
+    }
 
     public void CheckPlayer(Direction direction)
     {
         Vector2 pos = Toolkit.VectorSum(transform.position, Toolkit.DirectiontoVector(direction) / 1.95f);
         RaycastHit2D hit = Physics2D.Raycast(pos, Toolkit.DirectiontoVector(direction));
-        if(hit.collider != null && hit.collider.tag == "Player")
+        if (hit.collider != null && hit.collider.tag == "Player")
         {
+            if (((Vector2)hit.collider.transform.position - pos).SqrMagnitude() <= 1)
+                Debug.Log("sadadadasa");
             MoveToPosition = hit.collider.transform.position;
             if (coroutine == null)
             {
