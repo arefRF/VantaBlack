ba480221d27d7846e24b8790d1eabbe131e43499
Reza Mirzaee <rmirzaee95@gmail.com>
2017-09-02 13:10:00 +0430

    EnemyMessage added

---
+++ Assets/scripts/Enemy/EnemyMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMessage : MonoBehaviour {
+    /// <summary>
+    ///  Message Class for Enemy components communation
+    ///  position and direction are basic message variables
+    /// </summary>
+    public Vector2 position;
+    public Direction direction;
+    public enum MessageType {
+        Move,
+        KillPlayer,
+        FireLaser,
+
+    }
+}
