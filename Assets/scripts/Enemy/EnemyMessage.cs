b0d153cf41ff6bf2116dd2301cab890e5fd104ca
aref <valipour.aref@gmail.com>
2017-09-04 11:36:28 +0430

    enemy message constyructyor

--- Assets/scripts/Enemy/EnemyMessage.cs
+++ Assets/scripts/Enemy/EnemyMessage.cs
@@ -9,10 +9,32 @@ public class EnemyMessage : MonoBehaviour {
     /// </summary>
     public Vector2 position;
     public Direction direction;
+    public MessageType messagetype;
     public enum MessageType {
         Move,
         KillPlayer,
         FireLaser,
 
+    }
+
+    public EnemyMessage(MessageType messagetype)
+    {
+        this.messagetype = messagetype;
+    }
+    public EnemyMessage(MessageType messagetype, Vector2 position)
+    {
+        this.messagetype = messagetype;
+        this.position = position;
+    }
+    public EnemyMessage(MessageType messagetype, Direction direction)
+    {
+        this.messagetype = messagetype;
+        this.direction = direction;
+    }
+    public EnemyMessage(MessageType messagetype, Direction direction, Vector2 position)
+    {
+        this.messagetype = messagetype;
+        this.direction = direction;
+        this.position = position;
     }
 }
